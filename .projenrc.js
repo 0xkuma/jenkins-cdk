d1c4efd43acb1b099e6309c274838735b7c3cf7f
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-10-04 13:13:05 +0800

    Remove git workflows

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,8 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.44.0',
   defaultReleaseBranch: 'main',
   name: 'jenkins-cdk',
+  workflowGitIdentity: false,
+  github: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
