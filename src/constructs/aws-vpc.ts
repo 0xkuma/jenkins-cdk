7a9c207539a2b2c5298488cee4a4d506986b9f6d
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-10-07 01:49:42 +0800

    Add create security group func

--- src/constructs/aws-vpc.ts
+++ src/constructs/aws-vpc.ts
@@ -1,26 +1,17 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
-export interface VpcSecurityGroupIds {
-  [key: string]: string[];
-}
-
-export interface VpcSubnetIds {
-  [key: string]: string[];
+export interface AwsVpcSecurityGroupIngressRuleProps {
+  readonly peer: ec2.IPeer;
+  readonly connection: ec2.Port;
+  readonly description: string;
+  readonly remoteRule: boolean;
 }
 
 export interface AwsVpcSecurityGroupProps {
-  readonly name: string;
-  readonly tags?: {
-    [key: string]: string;
-  };
-}
-
-export interface AwsVpcSubnetProps {
-  readonly cidr: string[];
-  readonly tags?: {
-    [key: string]: string;
-  };
+  readonly allowAllOutbound?: boolean;
+  readonly description?: string;
+  readonly securityGroupName?: string;
 }
 
 export interface AwsVpcProps {
@@ -36,6 +27,14 @@ export interface AwsVpcProps {
 export class AwsVpc extends Construct {
   public readonly vpc: ec2.Vpc;
   public readonly getPublicSubnetIds: () => string[];
+  public readonly createSecurityGroup: (
+    type: string,
+    props: AwsVpcSecurityGroupProps,
+  ) => ec2.SecurityGroup;
+  public readonly addIngressRule: (
+    securityGroup: ec2.SecurityGroup,
+    props: AwsVpcSecurityGroupIngressRuleProps,
+  ) => void;
 
   constructor(scope: Construct, id: string, props: AwsVpcProps) {
     super(scope, id);
@@ -47,10 +46,42 @@ export class AwsVpc extends Construct {
       }
     });
 
+    this.vpc = new ec2.Vpc(this, 'VPC', exp);
+
     this.getPublicSubnetIds = () => {
       return this.vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }).subnetIds;
     };
 
-    this.vpc = new ec2.Vpc(this, 'VPC', exp);
+    this.createSecurityGroup = (type: string, funcProps: AwsVpcSecurityGroupProps) => {
+      let funcExp: { [key: string]: any } = {};
+      Object.entries(funcProps).forEach(([key, value]) => {
+        if (value !== undefined) {
+          funcExp[key] = value;
+        }
+      });
+      return new ec2.SecurityGroup(
+        this,
+        `${type}-SecurityGroup`,
+        Object.assign(funcExp, { vpc: this.vpc }),
+      );
+    };
+
+    this.addIngressRule = (
+      securityGroup: ec2.SecurityGroup,
+      funcProps: AwsVpcSecurityGroupIngressRuleProps,
+    ) => {
+      let funcExp: { [key: string]: any } = {};
+      Object.entries(funcProps).forEach(([key, value]) => {
+        if (value !== undefined) {
+          funcExp[key] = value;
+        }
+      });
+      securityGroup.addIngressRule(
+        funcExp.peer,
+        funcExp.connection,
+        funcExp.description,
+        funcExp.remoteRule,
+      );
+    };
   }
 }
