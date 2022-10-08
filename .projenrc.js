9b6d6b27be8e21e52b0a315d4fc72b782a6ef723
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-10-08 23:18:01 +0800

    Upgrade to es2020

--- .projenrc.js
+++ .projenrc.js
@@ -7,7 +7,12 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   github: false,
   gitignore: ['.env'],
   deps: ['dotenv'], /* Runtime dependencies of this module. */
-
+  tsconfig: {
+    compilerOptions: {
+      lib: ['es2020'],
+      target: 'es2020',
+    },
+  },
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
